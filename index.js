4635e89ef9e6c2fd658555bfb3c9fc0db8f1c03c
Sanjay Puri <39893694+sanjaypuri@users.noreply.github.com>
2023-09-26 20:27:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors(
+  {
+    origin:["http://127.0.0.1:3000", "http://192.168.1.52:3000"],
+    methods:["GET", "POST"],
+    credentials: true
+  }
+));
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    {
+      id:1,
+      name:"Sanjay"
+    },
+    {
+      id:2,
+      name:"Sanju"
+    },
+    {
+      id:3,
+      name:"Shubam"
+    },
+    {
+      id:4,
+      name:"Shobit"
+    },
+  ]);
+});
+
+app.listen(5000);
